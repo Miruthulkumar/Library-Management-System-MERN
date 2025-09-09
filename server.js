59b40354fbf8d0d2a390c23d61099d3cdfd77a6c
Miruthul kumar M <127958188+Miruthulkumar@users.noreply.github.com>
2025-09-09 21:17:12 +0530

    cmd fix

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
-const express = require("express"); // Importing the Express framework to create the server and handle routing
-const app = express(); // Initialize an Express application and assign it to the constant 'app'
-const PORT = 4000; // Define the port number on which the server will listen for incoming requests
+const express = require("express"); 
+const app = express(); 
+const PORT = 4000; 
 
-const connectDb = require("./db"); // Import the connectDb function from the db.js file to establish a database connection
-connectDb(); // Call the connectDb function to connect to the MongoDB database
+const connectDb = require("./db"); 
+connectDb(); 
 
-// Import the Todo schema/model from the todoschema.js file to interact with the todos collection in MongoDB
 const Book = require("./bookSchema");
-app.use(express.json()); // Middleware to parse incoming JSON payloads in request bodies
+app.use(express.json()); 
 
 //get all books
 app.get("/library/books", async (req, res) => {
